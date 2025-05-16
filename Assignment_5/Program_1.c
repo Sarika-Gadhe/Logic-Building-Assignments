60b9074c34be65fe68be9dba9657977f881f784f
Sarika Gadhe <sarikagadhe60@gmail.com>
2025-05-17 02:04:24 +0530

    Create Program_1.c

---
+++ Assignment_5/Program_1.c
@@ -0,0 +1,40 @@
+//_________________________________________________________________________________________________
+//
+//  File name :     Program1.c
+//  Description :   Used to Accept number from user and print that number of $ & * on screen.
+//  Author :        Sarika Devidas Gadhe
+//  Date :          16/05/2025
+//
+//_________________________________________________________________________________________________
+
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo <= 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <=iNo; iCnt++)
+    {
+        printf("$ * ");
+    }
+   
+ 
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+    
+    Pattern(iValue);
+
+    return 0;
+}
